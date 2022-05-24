e9736a46bbaa5dc70c8e708787b3170f4d0dc20c
12fiza <fizathahareen412@gmail.com>
2022-05-24 15:30:21 +0530

    first commit

--- corejava/src/abcpackage/Addition.java
+++ corejava/src/abcpackage/Addition.java
@@ -1,11 +1,15 @@
 package abcpackage;
 
 public class Addition {
-	public int addTwoNumbers(int x, int y) {
+	protected int addTwoNumbers(int x, int y) {
 		return x+y;
-
-			
-
 	}
-
+	class b
+	{
+		
+	}
+	public static void main(String[] args) {
+		//Addition ob1=new Addition();
+		//ob1.addTwoNumbers(10, 20);
+	}
 }
